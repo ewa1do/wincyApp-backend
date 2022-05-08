908f4e43332a127f9ab5673658c5d2d0355f9c66
ewa1do <this.eduardovera@gmail.com>
2022-05-08 12:31:44 -0400

    connection to mongodb database

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-app.listen(4000, '127.0.0.1', () => {
-  console.log('App running on Port: 4000');
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Database connection successful!'));
+
+const { PORT } = process.env;
+const hostname = '127.0.0.1';
+
+app.listen(PORT, hostname, () => {
+  console.log('App running on Port:', PORT);
 });
