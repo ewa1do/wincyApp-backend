6027c6f164101d7065113d7c68d858434d8f5f97
ewa1do <this.eduardovera@gmail.com>
2022-05-08 20:48:23 -0400

    Adding basic CRUD operations for the schoolars route

--- controllers/schoolarController.js
+++ controllers/schoolarController.js
@@ -1,4 +1,96 @@
-exports.getAllSchoolars = function (req, res, next) {
-  res.send('Testing route');
-  next();
+const Schoolar = require('../models/schoolarModel');
+
+exports.getAllSchoolars = async function (req, res) {
+  try {
+    const schoolars = await Schoolar.find();
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        schoolars,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.getSchoolar = async function (req, res) {
+  try {
+    const schoolar = await Schoolar.findById(req.params.id);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        schoolar,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.createSchoolar = async function (req, res) {
+  try {
+    const newSchoolar = await Schoolar.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        newSchoolar,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.updateSchoolar = async function (req, res) {
+  try {
+    const updateSchoolar = await Schoolar.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        updateSchoolar,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.deleteSchoolar = async function (req, res) {
+  try {
+    await Schoolar.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
--- routes/schoolarRouter.js
+++ routes/schoolarRouter.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const schoolarController = require('../controllers/schoolarController');
 
-const { getAllSchoolars } = schoolarController;
+const {
+  getAllSchoolars,
+  getSchoolar,
+  createSchoolar,
+  updateSchoolar,
+  deleteSchoolar,
+} = schoolarController;
 
 const router = express.Router();
 
-router.route('/').get(getAllSchoolars);
+router.route('/').get(getAllSchoolars).post(createSchoolar);
+
+router
+  .route('/:id')
+  .get(getSchoolar)
+  .patch(updateSchoolar)
+  .delete(deleteSchoolar);
 
 module.exports = router;
