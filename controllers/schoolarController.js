7e97f1bea97972f22a8aeebd6309617c65fd22b9
ewa1do <this.eduardovera@gmail.com>
2022-05-20 10:32:12 -0400

    adding 404 not found errors

--- controllers/schoolarController.js
+++ controllers/schoolarController.js
@@ -1,8 +1,9 @@
 const Schoolar = require('../models/schoolarModel');
 const APIFeatures = require('../utils/apiFeatures');
 const catchAsync = require('../utils/catchAsyncErros');
+const AppError = require('../utils/appError');
 
-exports.getAllSchoolars = catchAsync(async function (req, res) {
+exports.getAllSchoolars = catchAsync(async function (req, res, next) {
   const features = new APIFeatures(Schoolar.find(), req.query)
     .filter()
     .sort()
@@ -20,8 +21,14 @@ exports.getAllSchoolars = catchAsync(async function (req, res) {
   });
 });
 
-exports.getSchoolar = catchAsync(async function (req, res) {
+exports.getSchoolar = catchAsync(async function (req, res, next) {
   const schoolar = await Schoolar.findById(req.params.id);
+
+  if (!schoolar) {
+    return next(
+      new AppError(`No tour found with the ID: ${req.params.id}`, 404)
+    );
+  }
 
   res.status(200).json({
     status: 'success',
@@ -31,7 +38,7 @@ exports.getSchoolar = catchAsync(async function (req, res) {
   });
 });
 
-exports.createSchoolar = catchAsync(async function (req, res) {
+exports.createSchoolar = catchAsync(async function (req, res, next) {
   const newSchoolar = await Schoolar.create(req.body);
 
   res.status(201).json({
@@ -42,7 +49,7 @@ exports.createSchoolar = catchAsync(async function (req, res) {
   });
 });
 
-exports.updateSchoolar = catchAsync(async function (req, res) {
+exports.updateSchoolar = catchAsync(async function (req, res, next) {
   const updateSchoolar = await Schoolar.findByIdAndUpdate(
     req.params.id,
     req.body,
@@ -52,6 +59,12 @@ exports.updateSchoolar = catchAsync(async function (req, res) {
     }
   );
 
+  if (!updateSchoolar) {
+    return next(
+      new AppError(`No tour found with the ID: ${req.params.id}`, 404)
+    );
+  }
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -60,8 +73,14 @@ exports.updateSchoolar = catchAsync(async function (req, res) {
   });
 });
 
-exports.deleteSchoolar = catchAsync(async function (req, res) {
-  await Schoolar.findByIdAndDelete(req.params.id);
+exports.deleteSchoolar = catchAsync(async function (req, res, next) {
+  const schoolar = await Schoolar.findByIdAndDelete(req.params.id);
+
+  if (!schoolar) {
+    return next(
+      new AppError(`No tour found with the ID: ${req.params.id}`, 404)
+    );
+  }
 
   res.status(204).json({
     status: 'success',
