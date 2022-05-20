8a279692bec8834bc50fdfdf3f1e9ac077344ca5
ewa1do <this.eduardovera@gmail.com>
2022-05-20 09:47:18 -0400

    refactoring the error handlers

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+
 const schoolarRouter = require('./routes/schoolarRouter');
 
 const app = express();
@@ -16,12 +19,11 @@ app.use('/api/v1/schoolars', schoolarRouter);
 
 // handling unhandled routes
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server`,
-  });
-
-  next();
+  //  this helps me to create a detailed error
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
+
+// and then that detailed error is formatted in the http response using the globalErrorHandler
+app.use(globalErrorHandler);
 
 module.exports = app;
