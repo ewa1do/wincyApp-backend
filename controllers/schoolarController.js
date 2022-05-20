802df9efc00ffade70ef0a9ad230ea28cea05d22
ewa1do <this.eduardovera@gmail.com>
2022-05-20 10:20:39 -0400

    refactoring controllers

--- controllers/schoolarController.js
+++ controllers/schoolarController.js
@@ -1,104 +1,70 @@
 const Schoolar = require('../models/schoolarModel');
 const APIFeatures = require('../utils/apiFeatures');
+const catchAsync = require('../utils/catchAsyncErros');
 
-exports.getAllSchoolars = async function (req, res) {
-  try {
-    const features = new APIFeatures(Schoolar.find(), req.query)
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate();
+exports.getAllSchoolars = catchAsync(async function (req, res) {
+  const features = new APIFeatures(Schoolar.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
 
-    const schoolars = await features.query;
+  const schoolars = await features.query;
 
-    res.status(200).json({
-      status: 'success',
-      results: schoolars.length,
-      data: {
-        schoolars,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+  res.status(200).json({
+    status: 'success',
+    results: schoolars.length,
+    data: {
+      schoolars,
+    },
+  });
+});
 
-exports.getSchoolar = async function (req, res) {
-  try {
-    const schoolar = await Schoolar.findById(req.params.id);
+exports.getSchoolar = catchAsync(async function (req, res) {
+  const schoolar = await Schoolar.findById(req.params.id);
 
-    res.status(200).json({
-      status: 'success',
-      data: {
-        schoolar,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+  res.status(200).json({
+    status: 'success',
+    data: {
+      schoolar,
+    },
+  });
+});
 
-exports.createSchoolar = async function (req, res) {
-  try {
-    const newSchoolar = await Schoolar.create(req.body);
+exports.createSchoolar = catchAsync(async function (req, res) {
+  const newSchoolar = await Schoolar.create(req.body);
 
-    res.status(201).json({
-      status: 'success',
-      data: {
-        newSchoolar,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+  res.status(201).json({
+    status: 'success',
+    data: {
+      newSchoolar,
+    },
+  });
+});
 
-exports.updateSchoolar = async function (req, res) {
-  try {
-    const updateSchoolar = await Schoolar.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      {
-        new: true,
-        runValidators: true,
-      }
-    );
+exports.updateSchoolar = catchAsync(async function (req, res) {
+  const updateSchoolar = await Schoolar.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
 
-    res.status(200).json({
-      status: 'success',
-      data: {
-        updateSchoolar,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+  res.status(200).json({
+    status: 'success',
+    data: {
+      updateSchoolar,
+    },
+  });
+});
 
-exports.deleteSchoolar = async function (req, res) {
-  try {
-    await Schoolar.findByIdAndDelete(req.params.id);
+exports.deleteSchoolar = catchAsync(async function (req, res) {
+  await Schoolar.findByIdAndDelete(req.params.id);
 
-    res.status(204).json({
-      status: 'success',
-      data: null,
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
